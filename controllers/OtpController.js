ee9b3cf5ee9f9f8e6187b3c6ab32eace6dbd5c2a
Saket Ahlawat <96969714+Saket109@users.noreply.github.com>
2023-06-05 14:03:32 +0530

    added mobile authentication code
    Added sendMobileOtp and VerifyOtpMobile functions. Not fully completed few environmental variables and libraries has to be required.

--- controllers/OtpController.js
+++ controllers/OtpController.js
@@ -1,7 +1,18 @@
+      // BELOW LINE HAVE TO CONFIG FEW ENVIRONMENT VARIBLES
+// const {accountSid,authToken,twilioNumber,serviceId} = require('./config')
+      // BELOW LINE HAS TO BE ADDED FOR USING TWILIO 
+// const client = require('twilio')(accountSid,authToken)
+
 const bcrypt = require("bcrypt");
 const otpGenerator = require("otp-generator");
 const nodemailer = require("nodemailer");
 require("dotenv/config");
+
+const OTP = otpGenerator.generate(6, {
+    specialChars: false,
+    lowerCaseAlphabets: false,
+    upperCaseAlphabets: false,
+  });
 
 const sendemail = async (email, OTP) => {
   const transporter = nodemailer.createTransport({
@@ -33,11 +44,6 @@ module.exports.userExistance =  async(req,res) => {
 }
 
 module.exports.signUp = async (req, res) => {
-  const OTP = otpGenerator.generate(6, {
-    specialChars: false,
-    lowerCaseAlphabets: false,
-    upperCaseAlphabets: false,
-  });
   const email = req.body.email;
   console.log(OTP);
   const otp = new Otp({ email: email, otp: OTP });
@@ -70,3 +76,31 @@ module.exports.verifyOtp = async (req, res) => {
     return res.send(false);
   }
 };
+
+module.exports.verrifyOtpMobile = async(req,res) => {
+  const user_entered_otp = req.body.code;
+  if(user_entered_otp == OTP){
+    res.status(200).send("Verified Successfully ");
+  }else{
+    res.status(400).send("Not Verified ");
+  }
+}
+
+module.exports.userExistanceMobile =  async (req,res) => {
+  const user = await User.findOne({
+    mobile: req.body.mobile,
+  });
+  if (user) return res.send(true);
+  return res.send(false);
+}
+
+module.exports.sendMobileOtp = async (req,res)=>{
+  client.messages
+        .create({
+            body: `Your OTP Code from the PGI side is ${OTP}`,
+            to : `+${req.body.mobileNumber}`,
+            from : `${twilioNumber}`
+        }).then((data)=>{
+            res.status(200).send(data);
+        })
+}
